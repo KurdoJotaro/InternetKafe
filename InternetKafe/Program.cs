ecfa2f661b08d09a6da325bfba19d31b7d36c8cb
Killua0-0 <76758434+Killua0-0@users.noreply.github.com>
2026-06-08 04:09:30 +0300

    Projenin 1. asamasi bitti - Kodlar Eklendi

---
+++ InternetKafe/Program.cs
@@ -0,0 +1,17 @@
+namespace InternetKafe;
+
+static class Program
+{
+    [STAThread]
+    static void Main()
+    {
+        ApplicationConfiguration.Initialize();
+
+        var yonetici = new KafeYonetici();
+
+        yonetici.MusteriEkle(new Musteri { Ad = "Ahmett Yýlmaz", Yas = 22, UyelikVar = true });
+        yonetici.MusteriEkle(new Musteri { Ad = "Mehmet Kaya", Yas = 19, UyelikVar = false });
+
+        Application.Run(new AnaForm(yonetici));
+    }
+}
